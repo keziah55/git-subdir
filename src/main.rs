20daad747d8b96124ddb5984713eb8676e1cbe47
keziah55 <19816994+keziah55@users.noreply.github.com>
2024-10-11 14:49:40 +0100

    adding unit tests

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use reqwest;
 use scraper::{Html, Selector};
 use serde_json::Value;
 use std::{
-    fs,
+    fmt, fs,
     path::{Path, PathBuf},
 };
 
@@ -23,13 +23,81 @@ pub struct Cli {
     ignore_subdirs: bool,
 }
 
+struct GitHubUrl {
+    username: String,
+    repo_name: String,
+    branch: String,
+    path: PathBuf,
+}
+
+impl GitHubUrl {
+    /// Create new GitHubUrl instance
+    pub fn new(url: &String) -> GitHubUrl {
+        let prefix = "https://github.com/";
+        if !url.starts_with(prefix) {
+            panic!("'{}' is not a github url", url);
+        }
+
+        let url_parts: Vec<&str> = url.strip_prefix(prefix).unwrap().split("/").collect();
+        if url_parts.len() < 4 {
+            panic!("'{}' is not a url to a directory within a github repo", url);
+        }
+
+        if url_parts[2] != "tree" {
+            panic!("Cannot parse url '{}'", url);
+        }
+
+        let username = String::from(url_parts[0]);
+        let repo_name = String::from(url_parts[1]);
+        let branch = String::from(url_parts[3]);
+        let path = PathBuf::from(url_parts[4..].join("/"));
+
+        GitHubUrl {
+            username,
+            repo_name,
+            branch,
+            path,
+        }
+    }
+
+    /// Return url to directory
+    pub fn url(&self) -> String {
+        format!("https://github.com/{}/{}/tree/{}/{}", self.username,
+        self.repo_name,
+        self.branch,
+        self.path.to_str().unwrap())
+    }
+
+    pub fn join(&self, part: &str) -> GitHubUrl {
+        let new_url = format!("{}/{}", self.url(), part);
+        GitHubUrl::new(&new_url)
+    }
+
+    pub fn as_raw_url(&self) -> String {
+        format!("{}?raw=true", self.url())
+    }
+}
+
+impl fmt::Display for GitHubUrl {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "GitHubUrl {{\n  username: {},\n  repo: {},\n  branch: {},\n  path: {}\n}}",
+            self.username,
+            self.repo_name,
+            self.branch,
+            self.path.to_str().unwrap()
+        )
+    }
+}
+
 fn make_dir(path: &Path) {
     fs::create_dir_all(path)
         .unwrap_or_else(|_| panic!("Could not create dir '{}'", path.to_str().unwrap()));
 }
 
-fn get_git_dir(url: &String, output_path: &PathBuf, ignore_subdirs: bool) {
-    let text = reqwest::blocking::get(url).unwrap().text().unwrap();
+fn get_git_dir(url: &GitHubUrl, output_path: &PathBuf, ignore_subdirs: bool) {
+    let text = reqwest::blocking::get(url.url()).unwrap().text().unwrap();
 
     let document = Html::parse_document(&text);
     let selector =
@@ -51,7 +119,7 @@ fn get_git_dir(url: &String, output_path: &PathBuf, ignore_subdirs: bool) {
 }
 
 fn download(
-    base_url: &String,
+    base_url: &GitHubUrl,
     item_info: &serde_json::Map<String, serde_json::Value>,
     output_path: &PathBuf,
     ignore_subdirs: bool,
@@ -64,7 +132,8 @@ fn download(
     // currently writes files like 'tracks/activities/test/__init__.py'
     // so find a nice way of subtracting the end of the url from the beginning of the path
 
-    let url = format!("{}/{}", base_url.clone(), item_name);
+    // let url = format!("{}/{}", base_url.clone(), item_name);
+    let url = base_url.join(item_name);
 
     match item_type {
         "file" => download_file(base_url, item_path, output_path),
@@ -77,10 +146,10 @@ fn download(
     }
 }
 
-fn download_file(url: &String, item_path: &str, output_path: &PathBuf) {
-    let url = format!("{}?raw=true", url.clone());
+fn download_file(url: &GitHubUrl, item_path: &str, output_path: &PathBuf) {
+    let raw_url = url.as_raw_url(); //format!("{}?raw=true", url.clone());
 
-    let text = reqwest::blocking::get(url).unwrap().text().unwrap();
+    let text = reqwest::blocking::get(raw_url).unwrap().text().unwrap();
 
     let mut filename = PathBuf::from(output_path);
     filename.push(item_path);
@@ -94,8 +163,8 @@ fn download_file(url: &String, item_path: &str, output_path: &PathBuf) {
     println!("Downloaded '{}'", item_path);
 }
 
-pub fn get_from_cli(cli: Cli) {
-    let output_path = match cli.output {
+pub fn get_from_cli(url: &String, output: Option<String>, ignore_subdirs: bool) {
+    let output_path = match output {
         Some(path) => PathBuf::from(path),
         None => PathBuf::from("."),
     };
@@ -104,12 +173,48 @@ pub fn get_from_cli(cli: Cli) {
         make_dir(&output_path);
     }
 
-    get_git_dir(&cli.url, &output_path, cli.ignore_subdirs);
+    let url = GitHubUrl::new(url);
+
+    get_git_dir(&url, &output_path, ignore_subdirs);
 }
 
 fn main() {
     let cli = Cli::parse();
 
     // let url = String::from("https://github.com/gjf2a/midi_fundsp/tree/master/examples");
-    get_from_cli(cli);
+    get_from_cli(&cli.url, cli.output, cli.ignore_subdirs);
+}
+
+#[test]
+fn test_default_args() {
+    let url = String::from("https://github.com/keziah55/ABBAd_day/tree/master/ABBAd_day");
+
+    get_from_cli(&url, None, false);
+
+    let expected_path = PathBuf::from("ABBAd_day");
+    assert!(expected_path.exists());
+    let expected_files = vec!["ABBAd_day.ino", "fileArray.ino"];
+    for filename in expected_files {
+        assert!(expected_path.join(filename).exists());
+    }
+
+    
+}
+
+#[test]
+fn test_custom_output_path() {
+    let url = String::from("https://github.com/keziah55/git-subdir/tree/main/src");
+
+    let output_path = PathBuf::from("tmp_test");
+    let mut expected_path = output_path.clone();
+    let output_path_arg = Some(output_path.into_os_string().into_string().unwrap());
+
+
+    get_from_cli(&url, output_path_arg, false);
+
+    assert!(expected_path.exists());
+
+    expected_path.push("src/main.rs");
+    assert!(expected_path.exists());
+
 }
